8d4cd301fd8e0e52cee827f739485ab261bc5cd7
LI HAOZE <lihaoze2062@qq.com>
2019-05-13 12:25:08 +0800

    Change PID

--- BSP/Inc/USER_DEFINITION.h
+++ BSP/Inc/USER_DEFINITION.h
@@ -22,18 +22,18 @@
 #define CAN_CONTROL	//const current control 
 //#define PWM_CONTROL	//const speed control
 
-#define IF_NO1
+//#define IF_NO1
 
 #define CHASSIS_KP 1.5f
 #define CHASSIS_KI 0.1f
 #define CHASSIS_KD 0.0f
 
 #define GIMBAL_YAW_KP  101.5f
-#define GIMBAL_YAW_KI  0.7f
+#define GIMBAL_YAW_KI  0.1f
 #define GIMBAL_YAW_KD  425.5f
 
 #define GIMBAL_PIT_KP  122.6f
-#define GIMBAL_PIT_KI  3.7f
+#define GIMBAL_PIT_KI  0.1f
 #define GIMBAL_PIT_KD  170.8f
 
 
